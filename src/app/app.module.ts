83bf463962c5a7814c62fe774ea72f9ef211e9de
Onika Kim Asiao Dumbrique <kim.asiao@partners.w2m.com>
2022-05-12 13:36:37 +0200

    login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,51 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule, APP_INITIALIZER } from '@angular/core'
+import { LayoutModule } from '@angular/cdk/layout'
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 /* ANGULAR MATERIAL */
+import { MatCommonModule } from '@angular/material/core';
+import { MatAutocompleteModule } from '@angular/material/autocomplete'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet'
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
+import { MatButtonToggleModule } from '@angular/material/button-toggle'
 import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatMenuModule } from '@angular/material/menu'
+import { MatPaginatorModule } from '@angular/material/paginator'
+import { MatRadioModule } from '@angular/material/radio'
+import { MatSlideToggleModule } from '@angular/material/slide-toggle'
+import { MatSortModule } from '@angular/material/sort'
+import { MatRippleModule } from '@angular/material/core'
+import { MatSnackBarModule } from '@angular/material/snack-bar'
+import { MatSelectModule } from '@angular/material/select'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
+import { MatStepperModule } from '@angular/material/stepper'
+import { MatInputModule } from '@angular/material/input'
+import { MatTableModule } from '@angular/material/table'
+import { MatTabsModule } from '@angular/material/tabs'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatTooltipModule } from '@angular/material/tooltip'
+import { MatSliderModule } from '@angular/material/slider'
+import { MatExpansionModule } from '@angular/material/expansion'
+import { MatGridListModule } from '@angular/material/grid-list'
+import {MatNativeDateModule} from '@angular/material/core';
+import { MatTreeModule } from '@angular/material/tree'
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 /** COMPONENTS */
 import { HomeComponent } from './components/home/home.component';
@@ -17,7 +54,6 @@ import { RegisterComponent } from './components/register/register.component';
 import { MyAccountComponent } from './components/my-account/my-account.component';
 import { MyStatsComponent } from './components/my-stats/my-stats.component';
 import { HowToPlayComponent } from './components/how-to-play/how-to-play.component';
-import { MatStepperModule } from '@angular/material/stepper';
 
 @NgModule({
   declarations: [
@@ -36,7 +72,71 @@ import { MatStepperModule } from '@angular/material/stepper';
     BrowserAnimationsModule,
     MatCardModule,
     MatButtonModule,
-    MatStepperModule
+    MatStepperModule,
+    MatNativeDateModule,
+    MatSliderModule,
+    MatTooltipModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatTabsModule,
+    MatInputModule,
+    MatTableModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSnackBarModule,
+    MatRadioModule,
+    MatStepperModule,
+    AppRoutingModule,
+    MatSortModule,
+    MatMenuModule,
+    MatSelectModule,
+    MatProgressBarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatProgressSpinnerModule,
+    MatRippleModule,
+    MatPaginatorModule,
+    MatSlideToggleModule,
+    LayoutModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatBadgeModule,
+    MatBottomSheetModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatProgressBarModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatCommonModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatMenuModule,
+    MatPaginatorModule,
+    MatSliderModule,
+    MatTooltipModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatTableModule,
+    MatInputModule,
+    MatStepperModule,
+    MatRadioModule,
+    MatSlideToggleModule,
+    MatSortModule,
+    MatRippleModule,
+    MatSnackBarModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatProgressSpinnerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatTreeModule,
+    DragDropModule
 
   ],
   providers: [],
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,9 +1,5 @@
-import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-import { BreakpointObserver } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { StepperOrientation } from '@angular/cdk/stepper';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -11,21 +7,18 @@ import { StepperOrientation } from '@angular/cdk/stepper';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  isLinear = false;
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
 
-  firstFormGroup = this._formBuilder.group({
-    firstCtrl: ['', Validators.required],
-  });
-  secondFormGroup = this._formBuilder.group({
-    secondCtrl: ['', Validators.required],
-  });
-  thirdFormGroup = this._formBuilder.group({
-    thirdCtrl: ['', Validators.required],
-  });
-  stepperOrientation: Observable<StepperOrientation>;
+  constructor(private _formBuilder: FormBuilder) {}
 
-  constructor(private _formBuilder: FormBuilder, breakpointObserver: BreakpointObserver) {
-    this.stepperOrientation = breakpointObserver
-      .observe('(min-width: 800px)')
-      .pipe(map(({matches}) => (matches ? 'horizontal' : 'vertical')));
+  ngOnInit() {
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
   }
 }

6305e453fcca8e25f5c1e3ff7fcde62ce4330190
oasiao <74007996+oasiao@users.noreply.github.com>
2022-06-05 23:03:22 +0200

    fixed Delete acct

--- src/app/components/my-account/my-account.component.ts
+++ src/app/components/my-account/my-account.component.ts
@@ -8,23 +8,25 @@ import { BattleService } from 'src/app/services/battle.service';
 @Component({
   selector: 'app-my-account',
   templateUrl: './my-account.component.html',
-  styleUrls: ['./my-account.component.scss']
+  styleUrls: ['./my-account.component.scss'],
 })
 export class MyAccountComponent implements OnInit {
-
   public user: User;
   public userStat: UserStat;
   public userID: string;
   public score: number;
-  constructor(private _authService: AuthService, private router: Router, 
-    private route: ActivatedRoute, private _battleService: BattleService) {
+  constructor(
+    private _authService: AuthService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private _battleService: BattleService
+  ) {
     this.user = this.route.snapshot.data['users'].user;
     this.userStat = this.route.snapshot.data['userStats'].userStat;
     this.userID = this.user._id;
-    if (this.userStat !== null){
-      this.score = Math.round(this.userStat.score);      
+    if (this.userStat !== null) {
+      this.score = Math.round(this.userStat.score);
     }
-
   }
 
   ngOnInit(): void {
@@ -33,24 +35,25 @@ export class MyAccountComponent implements OnInit {
 
   public myAccountData(): void {
     let userLogged = JSON.parse(localStorage.getItem('userLogged'));
-    this._authService.getOneUser(userLogged.id).subscribe(
-      data => {
-        this.user = {
-          _id: data.user._id,
-          username: data.user.username,
-          email: data.user.email,
-          avatar: data.user.avatar
-        }
-      }
-    );
+    this._authService.getOneUser(userLogged.id).subscribe((data) => {
+      this.user = {
+        _id: data.user._id,
+        username: data.user.username,
+        email: data.user.email,
+        avatar: data.user.avatar,
+      };
+    });
   }
 
   public deleteProfile(id: string): void {
-    this._authService.deleteUser(id).subscribe(user => {
-      if (localStorage.getItem('userLogged') !== null){
+    this._authService.deleteUser(id).subscribe((user) => {
+      if (localStorage.getItem('userLogged') !== null) {
         localStorage.removeItem('userLogged');
       }
-      this.router.navigate([""]);
+
+      window.localStorage.clear();
+
+      this.router.navigate(['']);
     });
   }
 }
